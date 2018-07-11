b31e23deb9d5aff589310f572ccd8944e8307884
Wade Stewart <stew.wade@gmail.com>
2018-07-11 12:24:54 -0400

    added App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react'
 import AboutPage from './AboutPage'
 import ErrorPage from './ErrorPage'
+import HomePage from './HomePage'
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
+  constructor() {
+    super()
 
+    this.state = {
+      page: 'home'
+    }
+  }
+
+  setPageToHome(e) {
+    e.preventDefault()
+    
+    this.setState({
+      page: 'home'
+    })
+  }
+
+  setPageToAbout(e) {
+    e.preventDefault()
+
+    this.setState(P{
+      page: 'about'
+    })
+  }
+
+  render() {
+    let Page
+    let pageTitle
+
+    switch (this.state.page) {
+      case 'home':
+        Page = HomePage
+        pageTitle = "Home"
+        break
+      case 'about':
+        Page = AboutPage
+        pageTitle = "About"
+        break
+      default:
+        Page = ErrorPage
+        pageTitle = "Error"
+        break
+    }
+
+    return (
+      <div className="page-container">
+        <header>
+          <h1>Component Lifecycle</h1>
+          <nav>
+            <a href="/" onClick={this.setPageToHome}>Home</a>
+            <a href="/about" onCl={this.setPageToAbout}>About</a>
+          </nav>
+        </header>
+        <main>
+          <Page message={pageTitle} />
+        </main>
+      </div>
+    )
   }
 }
 
